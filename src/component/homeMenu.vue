972c141317988d0f94c7719fb76b290359d4a65c
yigechengzi <cxzlov3@163.com>
2019-07-04 19:28:51 +0800

    修改导航样式

--- src/component/homeMenu.vue
+++ src/component/homeMenu.vue
@@ -1,15 +1,19 @@
 <template>
     <ul class="menu-container" 
-        @mousemove="chenggeLeft">
-       <li class="menu-item" v-for="(item,index) in menuData" :key="index">
-           <a href="#" @click.prevent="changeMenu"  class="levelOneMenu">
+        @mousemove="changeMove">
+       <li class="menu-title"><span>cms-demo</span></li>
+       <li class="menu-item" 
+        :key="index" 
+        v-for="(item,index) in menuData" 
+        :class="{menuActive:index === menuIndex}">
+           <a href="#" class="levelOneMenu" @click="handleClickMenu(index,$event)">
                <i class="el-icon-eleme"></i>
                 <span>{{item}}</span>
                 <i class="el-icon-caret-bottom"></i>
            </a>
-           <dl>
-               <dd></dd>
-           </dl>
+           <ul class="submenu-item" v-show="index === menuIndex">
+               <li><a href="#">二级标题</a></li>
+           </ul>
        </li>
        <li class="leftBorder" :style="borderTop"></li>
     </ul>
@@ -26,19 +30,28 @@ export default {
             borderTop:{
                 top:0,
                 opacity:0
-            }
+            },
+            menuIndex:-1,
+
         }
     },
     methods:{
-        changeMenu(e){
-
-
+        handleClickMenu(index,e){
+            if(index === this.menuIndex){
+                this.menuIndex = -1
+                return
+            }
+            this.menuIndex = index
+            this.$nextTick(function(){
+                this.changeLeft(e.target)
+            })
         },
-        chenggeLeft(){
-            const target = event.target
-
-            if(target.tagName === 'LI'){
-                this.borderTop.top = getClienRect(target).y + 'px'
+        changeMove(){
+            this.changeLeft(event.target)
+        },
+        changeLeft(el){
+            if(Array.from(el.classList).indexOf('levelOneMenu') != -1){
+                this.borderTop.top = getClienRect(el).y + 'px'
                 this.borderTop.opacity = 1
             }
         }
@@ -46,17 +59,23 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
     .menu-container
         background-color #20222a
         height 100%
         color white
         position relative
+        .menu-title
+            padding 23px
+            text-align center
+            font-size 18px
+            border-bottom 1px solid rgba(0,0,0,0.3)
+        .menuActive
+            .el-icon-caret-bottom
+                transform rotateX(180deg)
         .menu-item
             font-size 14px
             color rgba(255,255,255,0.7)
-            padding 23px
-            cursor pointer
             &:hover
                 .levelOneMenu
                     color white
@@ -64,9 +83,15 @@ export default {
                 position relative
                 display block
                 color rgba(255,255,255,0.7)
-                .el-icon-caret-bottom
-                    position absolute
-                    right 0
+                padding 23px
+                
+            .submenu-item
+                background rgba(0,0,0,0.3)
+                padding 5px 0
+                a
+                    padding 15px
+                    padding-left 60px
+                    display block
         .leftBorder
             position absolute
             top 0
@@ -75,6 +100,11 @@ export default {
             height 60px
             background-color #009688
             transition all .3s
+
+.el-icon-caret-bottom
+    position absolute
+    transform rotateX(0)
+    right 15px
 </style>
 
 
